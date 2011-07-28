311cd8c91230e3dd5d71bb919d0bda27c0b880db
florind <anotherflo@yahoo.com>
2011-07-28 16:13:00 +0200

    Use JQuery 1.5.1, zombie is not compatible with 1.6.2

--- spec/zombie.js
+++ spec/zombie.js
@@ -3,8 +3,12 @@ var zombie = require('zombie'),
 		testCase = require('nodeunit').testCase;
 
 exports['Page should properly render'] =function(test) {
-	var browser = new zombie.Browser({debug: false})
+	var browser = new zombie.Browser({debug: true})
+	browser.runScripts = true;
 	browser.visit("http://localhost:4010", function(err, browser, status) {
+		if(err)	{
+			console.log(err.message)
+		}
 		test.equal(err, null)
 		test.equal(status, 200);
 		test.equal(browser.text("title"), "SuperUpload");	
