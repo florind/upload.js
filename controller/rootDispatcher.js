968313e62e02cd65402292ed9c5cd9c5745142d7
florind <florin@newsplore.com>
2011-08-13 08:16:35 +0200

    Introduced express.js for simplfying dispatching logic

--- controller/rootDispatcher.js
+++ controller/rootDispatcher.js
@@ -1,37 +1,45 @@
-var http = require('http'), formidable = require('formidable'), 
-  url = require('url'), path = require('path'), qs = require('querystring'),
-  storage = require('../lib/storage.js'), fs = require("fs"), sys=require('sys');
+var http = require('http'),	
+						express = require('express'),				// http://expressjs.com
+						formidable = require('formidable'),	// https://github.com/felixge/node-formidable
+						form = require('connect-form'),			// http://visionmedia.github.com/connect-form
+					  url = require('url'), path = require('path'), fs = require("fs"),
+					 	storage = require('../lib/storage.js');
 
 var superUploaderFile = fs.readFileSync('public/SuperUploader.html', 'utf-8');
-
-var server = http.createServer(function(req, res) {
-  var pathname = url.parse(req.url).pathname;
-  if(pathname == '/' && req.method == 'GET') {
-    res.writeHead(200, {'content-type' : 'text/html'});
-    res.end(superUploaderFile, 'utf-8');
-  } else if(pathname == '/upload' && req.method == 'POST') {
-    uploadFile(req, res);
-  } else if(pathname.match(/\/files\/[\w.]*$/) && req.method == 'GET') {  // path matching /files/{fileId}
-    serveFile(pathname, res);
-  } else if(pathname == '/attachment' && req.method == 'POST') {
-    attachContent(req, res);
-  } else {
-    sendResponse(res, 400, "Bad Request");
-  }
-});
+//express.js initialization
+var server = express.createServer(
+	form({
+		keepExtensions: true,
+		uploadDir: './filestore'
+	})
+);
+server.use(express.bodyParser());
 exports.server = server;
 
+//express.js dispatcher
+server.get('/', function(req, res) {
+  res.end(superUploaderFile, 'utf-8');	
+});
+server.post('/upload', function(req, res) {
+	if(req.form) {	//the form was successfully ubmitted
+		uploadFile(req, res);
+	} else {				//return Bad Request otherwise
+		sendResponse(res, 400, "Malformatted request data");
+	}
+});
+server.get('/files/:fileId', function(req, res) {
+	res.sendfile(storage.get(url.parse(req.url).pathname));
+});
+server.post('/attachment', function(req, res) {
+	attachContent(req, res);
+});
+
 function uploadFile(req, res) {
-  var form = new formidable.IncomingForm();
-  form.uploadDir = './filestore';
   var link = '';
-  form.addListener("file", function(name, file) {
+  req.form.on("file", function(name, file) {
     link = storage.put(file.path)
   });
-  form.addListener('error', function() {
-    sendResponse(res, 400, "Malformatted request data");
-  });
-  form.parse(req, function(err, fields, files) {
+  req.form.complete(function(err, fields, files) {
     res.writeHead(201, {
       'content-type' : 'text/plain',
       'Location' : link
@@ -40,32 +48,15 @@ function uploadFile(req, res) {
   });
 }
 
-function serveFile(pathname, res) {
-  if(storage.get(pathname) == null) {
-    sendResponse(res, 404, 'File not found.');    
-  } else {
-    fs.readFile(storage.get(pathname), function(err, data) {
-      if (err) sendResponse(res, 500, 'Internal Server Error');
-      res.writeHead(200, {'content-type' : 'application/binary'});
-      res.end(data);
-    });
-  }
-}
-
 function attachContent(req, res) {
-  var fullBody = '';
-    req.on('data', function(chunk) {
-    fullBody += chunk;
-    });
-  req.on('end', function() {
-    var posted = qs.parse(fullBody);
-    if(storage.get(posted.fileLink) == null) {  //front door check if the resource doesn't exist on the server
-      sendResponse(res, 400, "Attached resource not found on the server.");
-    }
+   if(storage.get(req.body.fileLink) == null) {  //front door check if the resource doesn't already exist on the server
+     sendResponse(res, 400, "Attached resource not found on the server.");
+   } else {
     res.writeHead(201, {'Content-Type': 'text/html'});
-    res.end("Success! Here's your file: <a href='" + posted.fileLink + "'>" + 
-    posted.uploadfile + "</a><br/><br/><a href='/'>Upload some more</a>");
-  });
+    res.end("Success! Here's your file: " +
+						"<a href='" + req.body.fileLink + "'>" + req.body.uploadfile + "</a>" +
+						"<br/><br/><a href='/'>Upload some more</a>");
+	}
 }
 
 function sendResponse(res, status, text) {
