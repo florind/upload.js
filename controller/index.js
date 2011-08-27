0fc7bb64e77889c9af7cf874498a7b0e6d1d9426
florind <anotherflo@yahoo.com>
2011-08-27 19:52:09 +0200

    Heroku port choice

--- controller/index.js
+++ controller/index.js
@@ -1,1 +1,3 @@
-require('./rootDispatcher.js').server.listen(8099);
+var port = process.env.PORT || 8099;
+console.log("Running on port: " + port);
+require('./rootDispatcher.js').server.listen(port);
