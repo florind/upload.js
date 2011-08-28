5d6df208e8de573f127c16e0b79b9c0dffac0ec4
florind <anotherflo@yahoo.com>
2011-08-28 15:47:51 +0200

    formatting

--- controller/rootDispatcher.js
+++ controller/rootDispatcher.js
@@ -12,29 +12,29 @@ var http = require('http'),
 var superUploaderFile;
 
 jade.renderFile('public/SuperUploader.jade', function(err, html) {
-	superUploaderFile = html;
+  superUploaderFile = html;
 });
 
 var server = express.createServer(form({
-	keepExtensions : true,
-	uploadDir : 'filestore'
+  keepExtensions : true,
+  uploadDir : 'filestore'
 }));
 server.use(express.bodyParser());
 exports.server = server;
 
-// request router
+//request router
 server.get('/', function(req, res) {
-	res.send(superUploaderFile);
+  res.send(superUploaderFile);
 });
 
 server.post('/upload', function(req, res) {
-	if (req.form) { // the form was successfully submitted
-		uploadFile(req, res);
-	} else { // otherwise return Bad Request
-		res.send('Malformatted request data', {
-			'Content-Type' : 'text/plain'
-		}, 400);
-	}
+  if (req.form) { // the form was successfully submitted
+    uploadFile(req, res);
+  } else { // otherwise return Bad Request
+    res.send('Malformatted request data', {
+      'Content-Type' : 'text/plain'
+    }, 400);
+  }
 });
 
 server.get('/files/:fileId', function(req, res) {
@@ -49,37 +49,37 @@ server.get('/files/:fileId', function(req, res) {
 });
 
 server.post('/attachment', function(req, res) {
-	attachContent(req, res);
+  attachContent(req, res);
 });
 
 function uploadFile(req, res) {
-	var link = '';
-	req.form.on("file", function(name, file) {
-		link = storage.put(file.path);
-	});
-	req.form.complete(function(err, fields, files) {
-		res.send('Success!', {
-			'Content-Type' : 'text/plain',
-			'Location' : link
-		}, 201);
-	});
+  var link = '';
+  req.form.on("file", function(name, file) {
+    link = storage.put(file.path);
+  });
+  req.form.complete(function(err, fields, files) {
+    res.send('Success!', {
+      'Content-Type' : 'text/plain',
+      'Location' : link
+    }, 201);
+  });
 }
 
 function attachContent(req, res) {
-	if (storage.get(req.body.fileLink) == null) { //front door check if the resource doesn't already exist on the server
-		res.send('Attached resource not found on the server.', {
-			'Content-Type' : 'text/plain'
-		}, 400);
-	} else {
-		jade.renderFile("public/UploadSuccess.jade", {
-			locals : {
-				fileLink : req.body.fileLink,
-				fileName : req.body.uploadfile
-			}
-		}, function(err, html) {
-			res.send(html, {
-				'Content-Type' : 'text/html'
-			}, 200);
-		});
-	}
+  if (storage.get(req.body.fileLink) == null) { //front door check if the resource doesn't already exist on the server
+    res.send('Attached resource not found on the server.', {
+      'Content-Type' : 'text/plain'
+    }, 400);
+  } else {
+    jade.renderFile("public/UploadSuccess.jade", {
+      locals : {
+        fileLink : req.body.fileLink,
+        fileName : req.body.uploadfile
+      }
+    }, function(err, html) {
+      res.send(html, {
+        'Content-Type' : 'text/html'
+      }, 200);
+    });
+  }
 }
