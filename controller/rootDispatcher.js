32336e4e52bc051d479add0fa1f42d12eee0c89e
florind <anotherflo@yahoo.com>
2011-08-28 12:55:34 +0200

    Downgraded jquery to 1.5.1

--- controller/rootDispatcher.js
+++ controller/rootDispatcher.js
@@ -51,17 +51,11 @@ function uploadFile(req, res) {
 	req.form.on("file", function(name, file) {
 		link = storage.put(file.path);
 	});
-	req.form.on("error", function(err) {
-    console.log(err);
-    res.send('Error!', {
-      'Content-Type' : 'text/plain',
-    }, 500);
-  });
 	req.form.complete(function(err, fields, files) {
 		res.send('Success!', {
 			'Content-Type' : 'text/plain',
 			'Location' : link
-		}, 201);	  
+		}, 201);
 	});
 }
 
